6e99741cecfb3a67b4bde5304b19206cda892e76
Sam Kissee <samkissee@gmail.com>
2021-01-22 11:55:53 -0700

    Initial Commit

---
+++ Shared/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by Sam Kissee on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
